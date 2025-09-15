025c418ded05f5af552cf758fd939913973207a0
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-09-15 12:54:30 +0530

    Some Changes

--- Day 5/5_RightRotateArrayByDPlace.java
+++ Day 5/5_RightRotateArrayByDPlace.java
@@ -4,26 +4,18 @@ class RightRotateArrayByDPlace {
         int k=4;
         int temp[]=new int[k];
 
-        int index1=0;
-        for(int i=k;i<nums.length ; i++){
-            temp[index1]=nums[i];
-            index1++;
-            
+        int n=nums.length;
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
         }
 
-        int index2=k;
-        for(int i=0;i<k ; i++){
-            nums[index2]=nums[i];
-            index2++;
+        for (int i = n - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
         }
 
-        for(int i=0;i<k ; i++){
-            nums[i]= temp[i];
-        }
-        
-
-        for(int i=0;i<nums.length ; i++){
-            System.out.print(nums[i]+", ");
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
         }
     }
 }
