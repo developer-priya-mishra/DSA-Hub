7c4c72e602fb4a116106f18eae9d6fb12b099628
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-09-17 11:26:30 +0530

    LeftRotateArrayByD

--- Day 7/2_LeftRotateArrayByDPosition.java
+++ Day 7/2_LeftRotateArrayByDPosition.java
@@ -27,3 +27,35 @@ class LeftRotateArrayByDPosition{
 
     }
 }
+
+
+class LeftRotateArrayByD{
+    static void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void rotateLeft(int arr[],int k){
+        int n=arr.length;
+        k=k%n;
+        reverse(arr,k,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,0,n-1);
+
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int k=3;
+
+        rotateLeft(arr,k);
+
+        for(int num : arr){
+            System.out.print(num+", ");
+        }
+    }
+}
