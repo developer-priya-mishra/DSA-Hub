db8036d81395c042c2284538d1d06168b0d5b083
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-09-17 11:19:22 +0530

    Efficient way of code added

--- Day 7/1_RightRotateArrayByDPosition.java
+++ Day 7/1_RightRotateArrayByDPosition.java
@@ -26,3 +26,36 @@ class RightRotateArrayByDPosition{
 
     }
 }
+
+
+class RightRotateArrayByD{
+
+    static void reverse(int arr[],int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void rightRotate(int arr[],int k){
+        int n=arr.length;
+        reverse(arr,n-k,n-1);
+        reverse(arr,0,n-k-1);
+        reverse(arr,0,n-1);
+    }
+
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int k=3;
+        k = k % arr.length;
+
+        rightRotate(arr,k);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
